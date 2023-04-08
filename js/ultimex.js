d9aa68d9621fd057008e6af62a063bd926310dd0
BOMBKILLA49 <129550633+BOMBKILLA49@users.noreply.github.com>
2023-04-08 01:57:53 -0600

    Update ultimex.js

--- js/ultimex.js
+++ js/ultimex.js
@@ -102,22 +102,3 @@ $(function() {
         responsive: {
             0: {
                 items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000:
-function printMessage(msg) {
-  console.log(msg);
-}
-
-let num1 = 5;
-let num2 = 10;
-
-if (num1 < num2) {
-  printMessage("num1 is less than num2");
-} else if (num1 > num2) {
-  printMessage("num1 is greater than num2");
-} else {
-  printMessage("num1 is equal to num2");
-}
